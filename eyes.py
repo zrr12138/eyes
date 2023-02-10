6fcc2368627ff88ca737c87c318a1fed18d53aaf
Ran.Zheng <ran.zheng@innogritcorp.com>
2023-02-10 17:39:39 +0800

    代码格式调整

--- eyes.py
+++ eyes.py
@@ -1,19 +1,23 @@
-#code by THGGDX
-#version:v1.0
+# code by THGGDX
+# version:v1.0
 import tkinter as tk
 from time import sleep
 import winsound
+
+
 def main():
     while True:
         sleep(1800)
-        win=tk.Tk()
-        win.wm_attributes("-topmost",1)
-        winsound.PlaySound("preview.mp3",winsound.SND_ASYNC)
+        win = tk.Tk()
+        win.wm_attributes("-topmost", 1)
+        winsound.PlaySound("preview.mp3", winsound.SND_ASYNC)
         win.title('护眼助手')
-        win.main=tk.Label(win,text="你已经使用电脑30分钟了,休息一下吧",font=("微软雅黑",15,"bold"))
+        win.main = tk.Label(win, text="你已经使用电脑30分钟了,休息一下吧", font=("微软雅黑", 15, "bold"))
         win.main.pack()
-        B = tk.Button(win,text="我知道了",command=win.destroy)
+        B = tk.Button(win, text="我知道了", command=win.destroy)
         B.pack()
         win.mainloop()
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     main()

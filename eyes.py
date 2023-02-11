b46c7aa6e8de58831143dd69e670785cb3680b66
Ran.Zheng <ran.zheng@innogritcorp.com>
2023-02-11 17:41:16 +0800

    如果用户正在敲击键盘或点击鼠标，那么提示会被延缓一段时间。（防止打扰到用户的使用，比如打游戏）

--- eyes.py
+++ eyes.py
@@ -1,22 +1,45 @@
-# code by THGGDX
-# version:v1.0
 import tkinter as tk
 from time import sleep
 import winsound
+from pynput import mouse, keyboard
+
+SLEEP_TIME = 5
+REMIND_TITLE = "eyes"
+REMIND_STRING = "You've been using the computer for 60 minutes, it's time to take a break"
+ANSWER_STRING = "I got it"
+
+
+def remind():
+    win = tk.Tk()
+    win.wm_attributes("-topmost", 1)  # means "Yes, do draw the window on top of all the others."
+    winsound.PlaySound("preview.mp3", winsound.SND_ASYNC)
+    win.title(REMIND_TITLE)
+    win.main = tk.Label(win, text=REMIND_STRING, font=("微软雅黑", 15, "bold"))
+    win.main.pack()
+    answer_button = tk.Button(win, text=ANSWER_STRING, command=win.destroy)
+    answer_button.pack()
+    win.mainloop()
+
+
+def delay_to_remind():
+    while True:
+        with keyboard.Events() as events:
+            # Block at most 5 second
+            event = events.get(5.0)
+            if event is not None:
+                continue
+        with mouse.Events() as events:
+            event = events.get(5.0)
+            if isinstance(event, mouse.Events.Click):
+                continue
+        break
 
 
 def main():
     while True:
-        #sleep(1800)
-        win = tk.Tk()
-        win.wm_attributes("-topmost", 1)
-        winsound.PlaySound("preview.mp3", winsound.SND_ASYNC)
-        win.title('护眼助手')
-        win.main = tk.Label(win, text="你已经使用电脑30分钟了,休息一下吧", font=("微软雅黑", 15, "bold"))
-        win.main.pack()
-        B = tk.Button(win, text="我知道了", command=win.destroy)
-        B.pack()
-        win.mainloop()
+        sleep(SLEEP_TIME)
+        delay_to_remind()
+        remind()
 
 
 if __name__ == '__main__':
